a2e999a6127743cb799c74287c0177ef3c2b0343
jawheelr <61429861+jawheelr@users.noreply.github.com>
2026-05-22 16:46:02 -0500

    Update blobDecryption.sh
    Added PBKDF2 compatibility for cross platform usage with Windows

--- scripts/blobDecryption.sh
+++ scripts/blobDecryption.sh
@@ -10,7 +10,6 @@
 # Parameters:
 # 1 - json BLOB
 #
-# ADD lines 15-88 into the top of your script
 ########################################################
 set -euo pipefail
 
@@ -26,59 +25,102 @@ PIN=""
 BLOB=${1}
 
 ########################################################
-# Functions
+# Cleanup
 ########################################################
-decode_json_blob() {
-  local BLOB="$1"
-  local PIN="$2"
+TMPFILE=""
+CTFILE=""
 
-  tmp=$(mktemp)
-  ct=$(mktemp)
-
-  printf "%s" "$BLOB" | base64 -d > "$tmp"
-
-  ########################################################
-  # Parse header
-  ########################################################
-  version=$(xxd -p -l 1 "$tmp")
-
-  iter_hex=$(xxd -p -s 1 -l 4 "$tmp" | \
-    fold -w2 | awk '{a[NR]=$0} END {for(i=NR;i>0;i--) printf "%s", a[i]}')
-
-  iterations=$((16#$iter_hex))
-
-  salt_hex=$(xxd -p -s 5 -l 16 "$tmp")
-  iv_hex=$(xxd -p -s 21 -l 16 "$tmp")
-
-  dd if="$tmp" of="$ct" bs=1 skip=37 status=none
-
-  ########################################################
-  # Key derivation (MUST MATCH ENCRYPTOR)
-  ########################################################
-  KEY=$(printf "%s|%s|%s" "$PIN" "$SALT" "$ITERATIONS" \
-  | openssl dgst -sha256 \
-  | awk '{print $2}')
-  
-  ########################################################
-  # Decrypt
-  ########################################################
-  plaintext=$(openssl enc -aes-256-cbc -d \
-    -K "$KEY" \
-    -iv "$iv_hex" \
-    -in "$ct")
-
-  printf "%s\n" "$plaintext"
-
-  rm -f "$tmp" "$ct"
+cleanup() {
+    [[ -f "${TMPFILE:-}" ]] && rm -f "$TMPFILE"
+    [[ -f "${CTFILE:-}" ]] && rm -f "$CTFILE"
 }
 
-########################################################
-# Run
-########################################################
-PAYLOAD_JSON=$(decode_json_blob "$BLOB" "$PIN")
+trap cleanup EXIT
 
 ########################################################
-# Assign user and password to hidden variables
+# Decode Blob
 ########################################################
-username=$(echo "$PAYLOAD_JSON" | jq -r '.secret.u')
-password=$(echo "$PAYLOAD_JSON" | jq -r '.secret.p')
+TMPFILE=$(mktemp)
+CTFILE=$(mktemp)
+
+printf "%s" "$BLOB" | base64 -d > "$TMPFILE"
+
+########################################################
+# Parse Header
+########################################################
+
+# Version byte
+VERSION=$(xxd -p -l 1 "$TMPFILE")
+
+# Iterations (little endian uint32)
+ITER_HEX_LE=$(xxd -p -s 1 -l 4 "$TMPFILE")
+
+ITER_HEX=$(echo "$ITER_HEX_LE" \
+    | sed 's/../& /g' \
+    | awk '{for(i=NF;i>=1;i--) printf $i}')
+
+ITERATIONS=$((16#$ITER_HEX))
+
+# Salt
+SALT_HEX=$(xxd -p -s 5 -l 16 "$TMPFILE")
+
+# IV
+IV_HEX=$(xxd -p -s 21 -l 16 "$TMPFILE")
+
+# Ciphertext starts at byte 37
+dd if="$TMPFILE" of="$CTFILE" bs=1 skip=37 status=none
+
+########################################################
+# Debug
+########################################################
+echo "Version:     $VERSION"
+echo "Iterations:  $ITERATIONS"
+echo "Salt:        $SALT_HEX"
+echo "IV:          $IV_HEX"
+
+########################################################
+# PBKDF2-HMAC-SHA1 Key Derivation
+# MUST MATCH PowerShell 5.1 Rfc2898DeriveBytes
+########################################################
+
+KEY_HEX=$(python3 - <<EOF
+import hashlib
+pin = "${PIN}".encode()
+salt = bytes.fromhex("${SALT_HEX}")
+iterations = ${ITERATIONS}
+
+key = hashlib.pbkdf2_hmac(
+    'sha1',
+    pin,
+    salt,
+    iterations,
+    32
+)
+
+print(key.hex())
+EOF
+)
+
+########################################################
+# Decrypt AES-256-CBC
+########################################################
+
+PLAINTEXT=$(openssl enc -aes-256-cbc -d \
+    -K "$KEY_HEX" \
+    -iv "$IV_HEX" \
+    -in "$CTFILE")
+
+########################################################
+# Output JSON
+########################################################
+echo
+echo "Decrypted JSON:"
+echo "$PLAINTEXT"
+
+########################################################
+# Example Usage
+########################################################
+# username=$(echo "$PAYLOAD_JSON" | jq -r '.secret.u')
+# password=$(echo "$PAYLOAD_JSON" | jq -r '.secret.p')
+USERNAME=$(echo "$PLAINTEXT" | jq -r '.secret.u')
+PASSWORD=$(echo "$PLAINTEXT" | jq -r '.secret.p')
